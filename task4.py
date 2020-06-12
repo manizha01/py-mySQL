aa1f524782fe923c3584b40576fcd98486fa7c12
manizha01 <manizha.razban@gmail.com>
2020-06-12 12:40:35 -0400

    first commit

---
+++ task4.py
@@ -0,0 +1,27 @@
+import mysql.connector
+
+mydb = mysql.connector.connect (
+    host = "localhost",
+    user = 'root',
+    passwd = '123qwe',
+    database = 'PyDB_Demo'
+)
+
+my_cursor = mydb.cursor()
+
+query = "UPDATE employees SET first_name ='arvin', last_name='anee', email='arin-an@yahoo.ca' WHERE EmpId=2" 
+
+my_cursor.execute(query)
+
+mydb.commit()
+
+my_cursor.execute('SELECT * FROM employees')
+for data in my_cursor:
+    print('Emp ID:', data[0])
+    print('Full name:', data[1], data[2])
+    print('Email:', data[3])
+    print('Job Title', data[4])
+    print('Hire Date', data[5])
+    print('Salary', data[6])
+
+print('==========================================================')
